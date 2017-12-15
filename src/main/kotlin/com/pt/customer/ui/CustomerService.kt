5ddfecd2a42e979749b6445688468beff7c2c21c
Petri Toivari <toivari@yahoo.com>
2017-12-15 16:05:44 +0200

    Add Metrics and support for spring cloud config

--- src/main/kotlin/com/pt/customer/ui/CustomerService.kt
+++ src/main/kotlin/com/pt/customer/ui/CustomerService.kt
@@ -1,36 +1,46 @@
 package com.pt.customer.ui
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
+import com.codahale.metrics.annotation.ExceptionMetered
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.core.ParameterizedTypeReference
+import org.springframework.http.HttpEntity
+import org.springframework.http.HttpMethod
+import org.springframework.http.ResponseEntity
+import org.springframework.stereotype.Service
+import org.springframework.web.client.RestTemplate
+import com.codahale.metrics.annotation.Timed
 
 @Service
 class CustomerService{
-    var log = LoggerFactory.getLogger(CustomerService::class.java)
+    val log = LoggerFactory.getLogger(CustomerService::class.java)
 
-    @Value("\${customerapi.url}")
+    @Value("\${config.customerApiUrl}")
     lateinit private var customerApiUrl: String
 
     @Autowired
     lateinit var customerRestTemplate: RestTemplate
 
+    @ExceptionMetered(name="findByNameFailed")
+    @Timed(name="findByName")
     fun findByName(name:String):List<Customer>{
         val url = getUrl("findByName/$name")
         val response = customerRestTemplate.exchange(url, HttpMethod.GET, null, object: ParameterizedTypeReference<List<Customer>>(){})
         return response.body
     }
 
+    @ExceptionMetered(name="getCustomerFailed")
+    @Timed(name="getCustomer")
     fun getCustomer(id:Long?):Customer? = customerRestTemplate.getForObject(getUrl("read/$id"), Customer::class.java)
 
+    @ExceptionMetered(name="deleteCustomerFailed")
+    @Timed(name="deleteCustomer")
     fun deleteCustomer(id:Long) = customerRestTemplate.delete(getUrl("delete/$id"))
 
+    @ExceptionMetered(name="saveCustomerFailed")
+    @Timed(name="saveCustomer")
     fun saveCustomer(customer:Customer) = customerRestTemplate.exchange(resolveSaveService(customer), HttpMethod.POST, HttpEntity(customer), Customer::class.java)
 
     private fun resolveSaveService(c:Customer) = if (c.id==null) getUrl("create") else getUrl("update")
--- src/main/kotlin/com/pt/customer/ui/MyCustomerVaadinApplication.kt
+++ src/main/kotlin/com/pt/customer/ui/MyCustomerVaadinApplication.kt
@@ -1,11 +1,28 @@
 package com.pt.customer.ui
 
+import com.ryantenney.metrics.spring.config.annotation.EnableMetrics
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.cloud.context.config.annotation.RefreshScope
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
 
+
+@EnableMetrics
 @SpringBootApplication
 class MyCustomerVaadinApplication
 
 fun main(args: Array<String>) {
     SpringApplication.run(MyCustomerVaadinApplication::class.java, *args)
+}
+
+
+@RefreshScope
+@RestController
+internal class MessageRestController {
+
+    @Value("\${boo:Hello default}")
+    @get:RequestMapping("/message")
+    val message: String? = null
 }
